667bc01aea386f799431a0c071b38b90c4361098
Upasana03 <upasanachoudhury2000@gmail.com>
2022-02-05 17:35:28 +0530

    new changes  Changes to be committed: 	new file:   Procfile 	modified:   app.js 	modified:   failure.html 	modified:   success.html

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 app.use(express.static("public"));
@@ -12,12 +13,56 @@ app.get("/",function(req,res){
     res.sendFile(__dirname + "/signup.html");
 });
 app.post("/",function(req,res){
-    var firstName = req.body.fname;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
-})
-app.listen(3000,function(){
+    const data = {
+        members:[
+          {
+            email_address: email,
+            status: "subscribed",
+            merge_fields:{
+              FNAME:firstName,
+              LNAME:lastName
+            }
+          }
+        ]
+      };
+
+    const jsonData = JSON.stringify(data);
+    const url = 'https://us14.api.mailchimp.com/3.0/lists/b0077e9264';
+
+    const options={
+        method:"POST",
+        auth:"upasana03:1936acfecdc9af928473ae2a06744b58-us14"
+      };
+
+
+      const request = https.request(url,options,function(response){
+        response.on("data",function(data){
+          sub_data = JSON.parse(data)
+          if(response.statusCode !== 200){
+            res.sendFile(__dirname+"/failure.html")
+          }else{
+            res.sendFile(__dirname+"/success.html")
+          }
+        });
+      });
+    
+      request.write(jsonData);
+      request.end();
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+  });
+  
+app.listen(process.env.PORT || 3000 ,function(){
     console.log("Server is running on port 3000");
 });
+
+// API KEY
+// 1936acfecdc9af928473ae2a06744b58-us14
+// list ID
+// b0077e9264
